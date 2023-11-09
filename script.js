058f18ac0c82c3c84e63399d8eda605271811360
Victor Volkov <vityan2504@gmail.com>
2023-11-09 21:17:49 +0300

    Написал игровой бот

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const guessNum = function(curNumber) {
+
+   const play = function() {
+      const num = prompt("Угадай число от 1 до 100");
+
+      if (num === null) {
+         alert("Игра окончена");
+         return;
+      }
+
+      if (isNaN(num) || num.trim() === "") {
+         alert("Введи число!");
+         play();
+      }
+
+      if (num > curNumber) {
+         alert("Загаданное число меньше");
+         play();
+      }
+
+      if (num < curNumber) {
+         alert("Загаданное число больше");
+         play();
+      }
+
+      if (+num === curNumber) {
+         alert("Поздравляю, Вы угадали!!!");
+      }
+   }
+
+   play();
+}
+
+guessNum(20);
