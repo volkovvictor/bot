562ca71cab5d3d96a7d0f747c4a106cf9dc3fec4
Victor Volkov <vityan2504@gmail.com>
2023-11-09 21:35:54 +0300

    Дописал функционал игрового бота

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 'use strict';
 
-const guessNum = function(curNumber) {
+const chances = 10;
+
+const guessNum = function(curNumber, count) {
 
    const play = function() {
-      const num = prompt("Угадай число от 1 до 100");
 
-      if (num === null) {
-         alert("Игра окончена");
-         return;
-      }
+      if (count > 0) {
+         const num = prompt("Угадай число от 1 до 100");
 
-      if (isNaN(num) || num.trim() === "") {
-         alert("Введи число!");
-         play();
-      }
+         if (num === null) {
+            alert("Игра окончена");
+            return;
+         }
 
-      if (num > curNumber) {
-         alert("Загаданное число меньше");
-         play();
-      }
+         if (isNaN(num) || num.trim() === "") {
+            alert("Введи число!");
+            play();
+         }
 
-      if (num < curNumber) {
-         alert("Загаданное число больше");
-         play();
-      }
+         if (num > curNumber && num !== null) {
+            alert("Загаданное число меньше");
+            count--;
+            play();
+         }
 
-      if (+num === curNumber) {
-         alert("Поздравляю, Вы угадали!!!");
+         if (num < curNumber && num !== null) {
+            alert("Загаданное число больше");
+            count--;
+            play();
+         }
+
+         if (+num === curNumber) {
+            const repeat = confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+
+            if (repeat === true) guessNum(20, chances);
+         }
+      } else {
+         const repeat = confirm("Попытки закончились, хотите сыграть еще?");
+
+         if (repeat === true) guessNum(20, chances);
       }
    }
 
    play();
 }
 
-guessNum(20);
+guessNum(20, chances);
